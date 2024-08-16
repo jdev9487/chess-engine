e724e7547d3333539ca80ad63c42516b59889bc6
John Gould <johngould9487@protonmail.com>
2024-08-16 17:18:44 +0100

    relocation block tests

--- Jdev.ChessEngine.Tests/Query/BlockedForRelocation.cs
+++ Jdev.ChessEngine.Tests/Query/BlockedForRelocation.cs
@@ -1,16 +1,65 @@
 namespace Jdev.ChessEngine.Tests.Query;
 
+using Moq;
+using Board;
+using Enums;
+using ChessEngine.Pieces;
+
 public class BlockedForRelocation : QueryBase
 {
+    private Mock<IPiece> _pieceToMove = default!;
+    private ISquare _destination = default!;
+    private ISquare _blockingLocation = default!;
+    
     [SetUp]
     public void BlockedForRelocationSetUp()
     {
-        
+        _destination = Square.At(File.F, Rank.Five);
+        _blockingLocation = Square.At(File.G, Rank.Two);
+        _pieceToMove = new Mock<IPiece>();
+        _pieceToMove.SetupGet(x => x.Colour).Returns(Colour.White);
+        _pieceToMove
+            .Setup(x => x.GetPotentialRelocationBlocks(_destination))
+            .Returns(new List<ISquare> { _blockingLocation });
     }
 
     [Test]
     public void ShouldBeBlocked()
     {
-        
+        var blockingPiece = new Mock<IPiece>();
+        blockingPiece.SetupGet(x => x.Colour).Returns(Colour.White);
+        PieceGroupMock
+            .Setup(x => x.PieceAt(_blockingLocation.File, _blockingLocation.Rank))
+            .Returns(blockingPiece.Object);
+        PieceGroupMock
+            .SetupGet(x => x.Pieces)
+            .Returns(new List<IPiece>
+            {
+                _pieceToMove.Object,
+                blockingPiece.Object
+            });
+
+        var actual = Query.IsPieceBlockedForRelocation(_destination, _pieceToMove.Object);
+        Assert.That(actual, Is.True);
+    }
+
+    [Test]
+    public void ShouldNotBeBlocked()
+    {
+        var blockingPiece = new Mock<IPiece>();
+        blockingPiece.SetupGet(x => x.Colour).Returns(Colour.White);
+        PieceGroupMock
+            .Setup(x => x.PieceAt(_blockingLocation.File, _blockingLocation.Rank))
+            .Returns((IPiece)null!);
+        PieceGroupMock
+            .SetupGet(x => x.Pieces)
+            .Returns(new List<IPiece>
+            {
+                _pieceToMove.Object,
+                blockingPiece.Object
+            });
+
+        var actual = Query.IsPieceBlockedForRelocation(_destination, _pieceToMove.Object);
+        Assert.That(actual, Is.False);
     }
 }
